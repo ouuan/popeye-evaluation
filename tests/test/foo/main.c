981bcf6f5017348bd347b79b1060993c6c407f58
Yufan You <ouuansteve@gmail.com>
2025-03-25 23:50:18 +0800

    Add example tests

---
+++ tests/test/foo/main.c
@@ -0,0 +1,22 @@
+#include <assert.h>
+#include <stdint.h>
+
+void *popeye_make_object(uint64_t size);
+
+void *popeye_make_message();
+
+uint16_t popeye_make_message_length();
+
+void parse(uint8_t *message, uint64_t size)
+{
+    assert(message[0] == message[1]);
+    assert(message[0] == message[2]);
+}
+
+int popeye_main()
+{
+    uint8_t *message = popeye_make_message();
+    uint64_t size = popeye_make_message_length();
+    parse(message, size);
+    return 0;
+}
