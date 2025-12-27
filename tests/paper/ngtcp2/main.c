57a478d81d116e4e5ae6d0acadb0203041533403
Yufan You <youyf21@mails.tsinghua.edu.cn>
2025-12-27 15:20:17 +0800

    Try old ngtcp2 version (currently stuck after simplify.dot dotted)

--- tests/paper/ngtcp2/main.c
+++ tests/paper/ngtcp2/main.c
@@ -14,10 +14,9 @@ int popeye_main()
     uint8_t *message = popeye_make_message();
     uint64_t size = popeye_make_message_length();
 
-    ngtcp2_frame_decoder frd;
-    ngtcp2_frame fr;
+    ngtcp2_max_frame *mfr = popeye_make_object(sizeof(ngtcp2_max_frame));
 
-    ngtcp2_ssize nread = ngtcp2_frame_decoder_decode(&frd, &fr, message, size);
+    ngtcp2_ssize nread = ngtcp2_pkt_decode_frame(&mfr->fr, message, size);
     assert(nread > 0);
 
     return 0;
